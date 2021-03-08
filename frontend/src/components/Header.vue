7f346491a2ea98f8107c8d171bda50751e921f42
Moritz Kitzmann <github@kitzmann.me>
2021-03-08 07:08:24 +0100

    add SearchBar component

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="flex justify-between items-center py-4 px-6 bg-white border-b-4 border-indigo-600"
+    class="flex justify-between items-center py-4 px-6"
   >
     <div class="flex items-center">
       <button
@@ -23,25 +23,7 @@
         </svg>
       </button>
 
-      <div class="relative mx-4 lg:mx-0">
-        <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
-          <svg class="h-5 w-5 text-gray-500" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            />
-          </svg>
-        </span>
-
-        <input
-          class="form-input w-32 sm:w-64 rounded-md pl-10 pr-4 focus:border-indigo-600"
-          type="text"
-          placeholder="Search"
-        />
-      </div>
+      <SearchBar />
     </div>
 
     <div class="flex items-center">
@@ -108,8 +90,10 @@
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import { useSidebar } from "../hooks/useSidebar";
+import SearchBar from "./SearchBar.vue";
 
 export default defineComponent({
+  components: {SearchBar},
   setup(_, { emit }) {
     const dropdownOpen = ref(false);
     const { isOpen } = useSidebar();
---
+++ frontend/src/components/NavigationItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="relative mx-4 lg:mx-0">
+        <span class="absolute inset-y-0 left-0 pl-3 flex items-center">
+          <svg class="h-5 w-5 text-gray-300" viewBox="0 0 24 24" fill="none">
+            <path
+                d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+          </svg>
+        </span>
+
+    <input
+        class="w-32 sm:w-64 rounded-md pl-10 pr-4 border-0 focus:border-indigo-300 focus:ring-2 focus:ring-indigo-200 focus:ring-opacity-50"
+        type="text"
+        placeholder="Search"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({})
+</script>
