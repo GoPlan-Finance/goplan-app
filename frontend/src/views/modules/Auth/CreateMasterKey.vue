ab01f7f595009ff5f551246c7d3f4e2a399bfee6
Moritz Kitzmann <github@kitzmann.me>
2021-03-09 11:06:48 +0100

    Improve styling in CreateMasterKey

--- frontend/src/views/modules/Auth/CreateMasterKey.vue
+++ frontend/src/views/modules/Auth/CreateMasterKey.vue
@@ -1,47 +1,48 @@
 <template>
   <form
-    class="mt-4"
     @submit.prevent="createKey"
   >
-    <label class="block">
+    <p
+      class="text-gray-700 text-sm mb-4"
+    >Its now time to create your Master Key. All your data will be encrypted with this key.</p>
+    <label class="block mb-4">
       <span
-        class="text-gray-700 text-sm"
-      >Its now time to create your Master Key. All your data will be encrypted with this key.</span>
-
+        class="text-gray-700 text-sm font-bold"
+      >Your Master Key</span>
       <input
         v-model="masterKey"
-        class="form-input mt-1 block w-full rounded-md focus:border-indigo-600"
+        class="form-input mt-2 bg-gray-100 border-1 border-gray-300 block w-full rounded-md focus:ring-2 focus:ring-blue-200 focus:ring-opacity-50"
         type="password"
       >
     </label>
 
     <label class="block">
-      <span class="text-gray-700 text-sm">Confirm your key</span>
+      <span class="text-gray-700 text-sm font-bold">Confirm your key</span>
       <input
         v-model="masterKey2"
-        class="form-input mt-1 block w-full rounded-md focus:border-indigo-600"
+        class="form-input mt-2 bg-gray-100 border-1 border-gray-300 block w-full rounded-md focus:ring-2 focus:ring-blue-200 focus:ring-opacity-50"
         type="password"
       >
     </label>
 
-    <div class="flex justify-between items-center mt-4">
+    <div class="flex justify-between items-center mt-6">
       <div>
-        <label class="inline-flex items-center">
+        <label class="inline-flex items-center cursor-pointer">
           <input
             v-model="acceptTesting"
-            class="form-checkbox text-indigo-600"
+            class="form-checkbox text-blue-600"
             type="checkbox"
           >
-          <span class="mx-2 text-gray-600 text-sm">I understand that this project is still a prototype and i accept all risks!</span>
+          <span class="mx-3 text-gray-600 text-sm">I understand that this project is still a prototype and i accept all risks!</span>
         </label>
       </div>
     </div>
 
-    <div class="mt-6">
+    <div class="mt-8">
       <button
         id="acceptTestingBtn"
         :disabled="!acceptTesting || masterKey.length < 7 || masterKey !== masterKey2"
-        class="py-2 px-4 text-center bg-indigo-600 rounded-md w-full text-white text-sm hover:bg-indigo-500"
+        class="py-2 px-4 text-center font-bold bg-blue-600 rounded-md w-full text-white text-sm hover:bg-blue-500"
         type="submit"
       >
         Sign in
