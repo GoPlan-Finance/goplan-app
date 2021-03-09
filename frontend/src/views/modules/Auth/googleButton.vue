c90eda7d21f656f3957bdade3d0a19b0bbef9e71
Moritz Kitzmann <github@kitzmann.me>
2021-03-09 10:33:02 +0100

    use tailwind for google button

--- frontend/src/views/modules/Auth/googleButton.vue
+++ frontend/src/views/modules/Auth/googleButton.vue
@@ -1,61 +1,22 @@
 <template>
   <button
-    class="googleSignIn"
-    @click="clicked"
+    class="flex items-center justify-center shadow-md hover:shadow-lg bg-blue-500 p-1 rounded-sm"
+    @click="$emit('clicked')"
   >
     <img
       alt="Google logo"
       src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/8399/G-on-white.svg"
     >
-    <span class="googleSignIn__text">Sign in with Google</span>
+    <span class="text-white px-8">Sign in with Google</span>
   </button>
 </template>
 
 <script>
-export default {
+import {defineComponent} from 'vue'
+
+export default defineComponent({
   emits: [
     'clicked'
-  ],
-  methods: {
-    clicked ()  {
-      this.$emit('clicked')
-    },
-  },
-}
+  ]
+})
 </script>
-
-<style scope>
-.googleSignIn {
-  width: 200px;
-  border: none;
-  border-radius: 2px;
-  background-color: #4285f4;
-  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.24), 0 0 1px 0 rgba(0, 0, 0, 0.12);
-  border: none;
-  display: flex;
-  padding: 1px;
-  color: #FFF;
-}
-
-.googleSignIn:hover {
-  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.24), 0 0 2px 0 rgba(0, 0, 0, 0.12);
-}
-
-.googleSignIn:active {
-  background: #3367D6;
-}
-
-
-.googleSignIn:disabled {
-  background: rgba(0, 0, 0, 0.08);
-  color: rgba(0, 0, 0, 0.4);
-  box-shadow: none;
-}
-
-.googleSignIn__text {
-  padding: 11px;
-  font-size: 14px;
-  font-weight: bold;
-  font-family: 'Roboto', arial, sans-serif;
-}
-</style>
