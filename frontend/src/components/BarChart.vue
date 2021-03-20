e2e57b8e987281fd5bea5c0742f737fb47b14eaa
Moritz Kitzmann <github@kitzmann.me>
2021-03-20 21:58:28 +0100

    Add chart examples

---
+++ frontend/src/components/BarChart.vue
@@ -0,0 +1,42 @@
+<template>
+  <apexchart
+    type="bar"
+    height="350"
+    :options="chartOptions"
+    :series="series"
+  />
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive, toRefs} from 'vue'
+
+export default defineComponent({
+  setup () {
+    const data = reactive({
+      chartOptions: {
+        chart: {
+          id: 'vuechart-example',
+        },
+        xaxis: {
+          categories: [
+            1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998
+          ],
+        },
+      },
+      series: [
+        {
+          name : 'series-1',
+          data : [
+            30, 40, 35, 50, 49, 60, 70, 91
+          ],
+        },
+      ],
+    })
+
+    return {
+      ...toRefs(data),
+    }
+  },
+})
+
+</script>
---
+++ frontend/src/components/DonutChart.vue
@@ -0,0 +1,45 @@
+<template>
+  <apexchart
+    type="donut"
+    height="350"
+    :options="chartOptions"
+    :series="series"
+  />
+</template>
+
+<script lang="ts">
+import {defineComponent, reactive, toRefs} from 'vue'
+
+export default defineComponent({
+  setup () {
+    const data = reactive({
+      chartOptions: {
+        chart: {
+          type: 'donut',
+        },
+        responsive: [
+          {
+            breakpoint : 480,
+            options    : {
+              chart: {
+                width: 200
+              },
+              legend: {
+                position: 'bottom'
+              }
+            }
+          }
+        ]
+      },
+      series: [
+        44, 55, 41, 17, 15
+      ],
+    })
+
+    return {
+      ...toRefs(data),
+    }
+  },
+})
+
+</script>
