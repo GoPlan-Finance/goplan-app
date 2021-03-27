613780fdaa98c72a4ff642898e52ac5adad22962
Moritz Kitzmann <github@kitzmann.me>
2021-03-27 13:45:09 +0100

    Add translations and improve watchlists

--- frontend/src/views/Watchlists.vue
+++ frontend/src/views/Watchlists.vue
@@ -1,47 +1,51 @@
 <template>
   <div>
-    <h1 class="text-gray-700 text-3xl font-medium">
+    <h1 class="text-gray-700 text-3xl font-medium mb-6">
       Watchlists
     </h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div
+        v-for="watchlist of watchlists"
+        :key="watchlist.slug"
+      >
+        <div class="shadow-md overflow-hidden rounded-lg">
+          <div class="bg-white px-6 py-4">
+            <div class="text-gray-900 font-bold text-xl mb-1">
+              {{ watchlist.title }}
+            </div>
+            <div class="text-gray-500  text-sm mb-2">
+              {{ $t('watchlists.updated') }} {{ watchlist.update.fromNow() }}
+            </div>
 
-    <div
-      v-for="watchlist in watchlists"
-      class="mt-4 mb-3"
-    >
-      <div class="shadow-lg max-w-sm overflow-hidden mt-6">
-        <div class="rounded-t-lg bg-white px-6 py-4">
-          <div class="text-gray-900 font-bold text-xl mb-1">
-            {{ watchlist.title }}
+
+            <div
+              class="min-w-min p-3 text-xl rounded-lg font-bold"
+              :class="watchlist.percentChange >= 0 ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'"
+            >
+              <span v-if="watchlist.percentChange >= 0">+</span>{{ watchlist.percentChange.toFixed(2) }} %
+            </div>
           </div>
-          <div class="text-gray-500  text-sm mb-2">
-            updated {{ watchlist.update }}
-          </div>
-
-
-          <div
-            class="min-w-min p-3 text-xl rounded-lg font-bold"
-            :class="changeIsPositive ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'"
+          <a
+            :href="'watchlists/' + watchlist.slug"
+            class="block px-6 py-4 bg-gray-50 text-gray-500"
           >
-            <span v-if="changeIsPositive">+</span> {{ percent.toFixed(2) }} %
-          </div>
-        </div>
-        <div class="rounded-b-lg bg-white px-6 py-4 bg-gray-50 text-blue-500">
-          <a href="">Show More</a>
+            {{ $t('watchlists.show_more') }}
+          </a>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent, ref } from 'vue'
-import dayjs, {DayJs} from 'dayjs'
+import dayjs from 'dayjs'
 
-type WatchlistItem = {
+export type WatchlistItem = {
   title: string,
   slug: string,
-  update: DayJs,
-  percentChange: number,
+  update: dayjs.DayJs,
+  percentChange: number
 }
 
 export default defineComponent({
@@ -50,16 +54,42 @@ export default defineComponent({
       {
         title         : 'Test Watchlist',
         slug          : 'test-watchlist',
-        update        : dayjs(new Date),
-        percentChange : 2.4
-      }
+        update        : dayjs(new Date).subtract('10', 'days'),
+        percentChange : 2.4,
+      },
+      {
+        title         : 'Big Tech Watchlist',
+        slug          : 'big-tech-watchlist',
+        update        : dayjs(new Date).subtract('10', 'weeks'),
+        percentChange : -12.52,
+      },
+      {
+        title         : 'Test Watchlist',
+        slug          : 'test-watchlist',
+        update        : dayjs(new Date).subtract('10', 'months'),
+        percentChange : 2.4,
+      },
+      {
+        title         : 'Big Tech Watchlist',
+        slug          : 'big-tech-watchlist',
+        update        : dayjs(new Date).subtract('1', 'year'),
+        percentChange : -12.52,
+      },
+      {
+        title         : 'Test Watchlist',
+        slug          : 'test-watchlist',
+        update        : dayjs(new Date).subtract('3', 'years'),
+        percentChange : 2.4,
+      },
+      {
+        title         : 'Big Tech Watchlist',
+        slug          : 'big-tech-watchlist',
+        update        : dayjs(new Date).subtract('3', 'month'),
+        percentChange : -12.52,
+      },
     ])
-    const changeIsPositive            = true
-    const percent                     = ref(2)
     return {
-      watchlists,
-      changeIsPositive,
-      percent
+      watchlists
     }
   },
 })
