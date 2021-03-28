8f0755013bf10c9bb0332b1f45b191d007b8d3c7
Moritz Kitzmann <github@kitzmann.me>
2021-03-28 12:41:03 +0200

    add default button

---
+++ frontend/src/components/base/ButtonDefault.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="flex gap-2 px-6 py-3 bg-blue-300 rounded-lg text-blue-600 font-bold tracking-wide hover:bg-blue-400"
+    @click="$emit('click', $event)"
+  >
+    <slot
+      name="before"
+    />{{ label }}<slot
+      name="after"
+    />
+  </button>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+
+export default defineComponent({
+  props: {
+    label: {
+      type     : String,
+      required : true,
+    },
+  },
+  emits: [
+    'click'
+  ],
+})
+</script>
