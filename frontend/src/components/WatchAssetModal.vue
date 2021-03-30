bab78279bff7f1938db2678c21c831999ca24ed3
Moritz Kitzmann <github@kitzmann.me>
2021-03-30 09:20:58 +0200

    Squashed commit of the following:
    commit 09988a1410797664ec2da25dda8745389e3a3959
    Author: Moritz Kitzmann <github@kitzmann.me>
    Date:   Tue Mar 30 09:20:09 2021 +0200
    
        improvements
    
    commit 8b3cddcf9de55ca25c68e1ba998c10688823212c
    Author: Moritz Kitzmann <github@kitzmann.me>
    Date:   Tue Mar 30 09:04:38 2021 +0200
    
        use close method in WatchAssetModal
    
    commit 6cd8fdecbdb1dcb35569a24913443bafb0186bb4
    Author: Moritz Kitzmann <github@kitzmann.me>
    Date:   Tue Mar 30 09:04:24 2021 +0200
    
        add close method to Modal component and expose as slotProp
    
    commit b1c4703ffb9ea5e61f82a8a4f8a7f2e848e7d368
    Author: Moritz Kitzmann <github@kitzmann.me>
    Date:   Tue Mar 30 09:04:03 2021 +0200
    
        Fix button vertical alignment
    
    commit 475e18805e6904d0f8ed0774c1a23c6e682234f4
    Author: Moritz Kitzmann <github@kitzmann.me>
    Date:   Tue Mar 30 08:04:09 2021 +0200
    
        add WatchAssetModal component

---
+++ frontend/src/components/WatchAssetModal.vue
@@ -0,0 +1,96 @@
+<template>
+  <Modal
+    title="Add to Watchlist"
+  >
+    <template #button>
+      <ButtonDefault
+        label="Watch"
+      >
+        <template #before>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="w-5 h-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+            <path
+              fill-rule="evenodd"
+              d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </template>
+      </ButtonDefault>
+    </template>
+    <template #content>
+      <div
+        v-for="(watchlist, index) in watchlists"
+        :key="index"
+      >
+        <div
+          class="cursor-pointer bg-gray-100 px-4 py-2 rounded-lg hover:bg-gray-300"
+          @click="addToWatchlist(watchlist)"
+        >
+          {{ watchlist.get('name') }}
+        </div>
+      </div>
+    </template>
+    <template #actions="slotProps">
+      <ButtonDefault
+        label="Close"
+        @click="slotProps.close()"
+      />
+    </template>
+  </Modal>
+</template>
+
+<script lang="ts">
+
+import {defineComponent, onBeforeMount, onUnmounted, ref} from 'vue'
+import {AssetSymbol} from '../../../common/models'
+import ButtonDefault from './base/ButtonDefault.vue'
+import Modal from './Modal.vue'
+import {Watchlist} from '../../../common/models/Watchlist'
+
+
+export default defineComponent({
+  components : {Modal, ButtonDefault},
+  props      : {
+    assetSymbol: {
+      type     : AssetSymbol,
+      required : true,
+    },
+  },
+  setup (props) {
+    let liveSubscription = null
+
+    const watchlists: Watchlist[] = ref([])
+
+    const addToWatchlist = async watchlist => {
+      watchlist.relation('symbols').add(props.assetSymbol)
+      await watchlist.save()
+      alert('added')
+    }
+
+    onBeforeMount(async () => {
+      const q          = new Parse.Query(Watchlist)
+      liveSubscription = await Watchlist.liveQuery(q, watchlists.value)
+    })
+
+    onUnmounted(async () => {
+      if (liveSubscription) {
+        await liveSubscription.unsubscribe()
+      }
+    })
+
+
+    return {
+      watchlists,
+      addToWatchlist
+    }
+  }
+
+
+})
+</script>
