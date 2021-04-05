147b207fcbf93812d16c30d65284d6afc3ada576
Moritz Kitzmann <github@kitzmann.me>
2021-04-05 16:49:15 +0200

    Replace svgs with heroicons

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -8,7 +8,7 @@
         @click="isOpen = true"
       >
         <svg
-          class="h-6 w-6"
+          class="h- w-6"
           fill="none"
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
@@ -28,20 +28,9 @@
 
     <div class="flex items-center">
       <button class="flex mx-4 text-gray-600 focus:outline-none">
-        <svg
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          />
-        </svg>
+        <BellIcon
+          class="h-7 w-7"
+        />
       </button>
 
       <div class="relative">
@@ -100,9 +89,10 @@ import {useSidebar} from '../hooks/useSidebar'
 import {useRouter} from 'vue-router'
 import SearchBar from './SearchBar.vue'
 import {AuthStore} from '@/store'
+import { BellIcon } from '@heroicons/vue/outline'
 
 export default defineComponent({
-  components: {SearchBar},
+  components: {SearchBar, BellIcon},
   // eslint-disable-next-line no-unused-vars
   setup () {
     const {push: pushRoute} = useRouter()
--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -21,62 +21,22 @@
         </router-link>
       </div>
 
-      <nav class="mt-10">
+      <nav class="mt-14">
         <navigation-item
           title="Watchlists"
           to="/watchlists"
         >
-          <svg
+          <TrendingUpIcon
             class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5 3C3.89543 3 3 3.89543 3 5V7C3 8.10457 3.89543 9 5 9H7C8.10457 9 9 8.10457 9 7V5C9 3.89543 8.10457 3 7 3H5Z"
-              fill="currentColor"
-            />
-            <path
-              d="M5 11C3.89543 11 3 11.8954 3 13V15C3 16.1046 3.89543 17 5 17H7C8.10457 17 9 16.1046 9 15V13C9 11.8954 8.10457 11 7 11H5Z"
-              fill="currentColor"
-            />
-            <path
-              d="M11 5C11 3.89543 11.8954 3 13 3H15C16.1046 3 17 3.89543 17 5V7C17 8.10457 16.1046 9 15 9H13C11.8954 9 11 8.10457 11 7V5Z"
-              fill="currentColor"
-            />
-            <path
-              d="M11 13C11 11.8954 11.8954 11 13 11H15C16.1046 11 17 11.8954 17 13V15C17 16.1046 16.1046 17 15 17H13C11.8954 17 11 16.1046 11 15V13Z"
-              fill="currentColor"
-            />
-          </svg>
+          />
         </navigation-item>
         <navigation-item
           title="Transactions"
           to="/transactions"
         >
-          <svg
+          <RefreshIcon
             class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M5 3C3.89543 3 3 3.89543 3 5V7C3 8.10457 3.89543 9 5 9H7C8.10457 9 9 8.10457 9 7V5C9 3.89543 8.10457 3 7 3H5Z"
-              fill="currentColor"
-            />
-            <path
-              d="M5 11C3.89543 11 3 11.8954 3 13V15C3 16.1046 3.89543 17 5 17H7C8.10457 17 9 16.1046 9 15V13C9 11.8954 8.10457 11 7 11H5Z"
-              fill="currentColor"
-            />
-            <path
-              d="M11 5C11 3.89543 11.8954 3 13 3H15C16.1046 3 17 3.89543 17 5V7C17 8.10457 16.1046 9 15 9H13C11.8954 9 11 8.10457 11 7V5Z"
-              fill="currentColor"
-            />
-            <path
-              d="M11 13C11 11.8954 11.8954 11 13 11H15C16.1046 11 17 11.8954 17 13V15C17 16.1046 16.1046 17 15 17H13C11.8954 17 11 16.1046 11 15V13Z"
-              fill="currentColor"
-            />
-          </svg>
+          />
         </navigation-item>
       </nav>
     </div>
@@ -88,9 +48,10 @@ import {defineComponent} from 'vue'
 import {useSidebar} from '../hooks/useSidebar'
 import NavigationItem from './NavigationItem.vue'
 import GoPlanLogo from './GoPlanLogo.vue'
+import { TrendingUpIcon, RefreshIcon } from '@heroicons/vue/outline'
 
 export default defineComponent({
-  components: {GoPlanLogo, NavigationItem},
+  components: {GoPlanLogo, NavigationItem, TrendingUpIcon, RefreshIcon},
   setup () {
     const {isOpen} = useSidebar()
 
