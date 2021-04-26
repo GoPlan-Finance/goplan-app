98c79f9a9efd557ce2224d854474ed8f70c22a35
Moritz Kitzmann <github@kitzmann.me>
2021-04-26 06:59:17 +0200

    Globally register GoIcons component

--- frontend/src/styleguide/views/Styleguide.vue
+++ frontend/src/styleguide/views/Styleguide.vue
@@ -3,22 +3,14 @@
     Styleguide
   </h1>
 
-  <GoIcons :name="icons.PENCIL" class="h-5 w-5"></GoIcons>
-  <GoIcons :name="icons.TRASH" type="outline" class="h-5 w-5"></GoIcons>
-  <GoIcons :name="icons.ACADEMIC_CAP" type="outline" class="h-10 w-10 transform rotate-180"></GoIcons>
+  <GoIcons name="PencilAlt" class="h-5 w-5"></GoIcons>
+  <GoIcons name="Trash" type="outline" class="h-5 w-5 hover:text-red-500 cursor-pointer"></GoIcons>
+  <GoIcons name="AcademicCap" type="outline" class="h-10 w-10 transform rotate-180"></GoIcons>
 </template>
 
 <script lang="ts">
-import GoIcons from '/@components/Icons/GoIcons.vue'
-import { icons } from '/@components/Icons/icons'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  components : {GoIcons},
-  setup() {
-    return {
-      icons
-    }
-  }
 })
 </script>
