6a259b37a75816e0cf44d57e8bf4c79aadfaca9c
mkitzmann <mkitzmann@gmail.com>
2022-01-09 14:34:56 +0100

    Add EditCreteAccount and Accounts table

---
+++ frontend/src/components/EditCreateAccount.vue
@@ -0,0 +1,124 @@
+<template>
+  <Modal
+    v-model="isModalOpen"
+    :title="account?.id ? t('Edit Account') : t('Create Account')"
+    @opened="modalOpened"
+  >
+    <template #button>
+      <slot name="button">
+        <ButtonDefault :label="account?.id ? t('Edit') : t('Create Account')">
+          <template v-if="!account?.id" #before>
+            <PlusCircleIcon class="h-6 w-6" />
+          </template>
+        </ButtonDefault>
+      </slot>
+    </template>
+    <template #content>
+      <div class="grid grid-cols-1 gap-x-3 gap-y-5">
+        <GFormItem :label="t('Name')" :error="errors.name">
+          <input v-model="accountInternal.name" class="rounded w-full" type="text" />
+        </GFormItem>
+        <GFormItem :label="t('Currency')" :error="errors.currency">
+          <input v-model="accountInternal.currency" class="rounded w-full" type="text" />
+        </GFormItem>
+      </div>
+    </template>
+    <template #actions>
+      <template v-if="account?.id">
+        <ButtonDefault :label="t('Save')" @click="save">
+          <template #before>
+            <SaveIcon class="h-6 w-6" />
+          </template>
+        </ButtonDefault>
+        <ButtonDefault type="secondary" :label="t('Delete')" @click="remove">
+          <template #before>
+            <TrashIcon class="h-6 w-6" />
+          </template>
+        </ButtonDefault>
+      </template>
+      <template v-else>
+        <ButtonDefault :label="t('Create')" @click="save">
+          <template #before>
+            <PlusCircleIcon class="h-6 w-6" />
+          </template>
+        </ButtonDefault>
+      </template>
+    </template>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import { Account } from '@common/models';
+import { Query } from '@goplan-finance/utils';
+import Modal from '@components/base/GoModal.vue';
+import { computed, reactive, ref } from 'vue';
+import ButtonDefault from './base/ButtonDefault.vue';
+import { PlusCircleIcon, TrashIcon, SaveIcon } from '@heroicons/vue/outline';
+import { useI18n } from 'vue-i18n';
+import GFormItem from '@components/base/GFormItem.vue';
+
+const { t } = useI18n();
+
+const props = defineProps<{
+  account?: Account;
+}>();
+
+const createAccount = (): Account => {
+  const newAccount = new Account();
+  newAccount.currency = 'USD';
+  return newAccount;
+};
+
+const errors = reactive({
+  name: null,
+  currency: null,
+});
+const accountInternal = ref<Account>(props.account ?? createAccount());
+const isModalOpen = ref(false);
+
+const reset = () => {
+  accountInternal.value = createAccount();
+  isModalOpen.value = false;
+};
+
+const isValid = computed<boolean>(() => {
+  if (!accountInternal.value.name) {
+    errors.name = t('Please input a name');
+  } else {
+    errors.name = null;
+  }
+
+  if (!accountInternal.value.currency) {
+    errors.currency = t('Please provide a currency');
+  } else {
+    errors.currency = null;
+  }
+
+  return Object.values(errors).filter(error => error !== null).length <= 0;
+});
+
+const save = async () => {
+  if (!isValid.value) {
+    return;
+  }
+
+  try {
+    await accountInternal.value.save();
+    reset();
+  } catch (e) {
+    alert(e);
+  }
+};
+
+const modalOpened = async () => {
+  if (props.account) {
+    accountInternal.value = await Query.create(Account).getObjectById(props.account.id);
+  }
+};
+
+const remove = (account: Account) => {
+  if (confirm(t('Do you really want to remove this account?'))) {
+    account.destroy();
+  }
+};
+</script>
--- frontend/src/views/Accounts.vue
+++ frontend/src/views/Accounts.vue
@@ -1,3 +1,60 @@
-<template>accounts</template>
+<template>
+  <HeadlineActions :headline="t('accounts.headline')">
+    <EditCreateAccount />
+  </HeadlineActions>
+  <template v-if="rows.length > 0">
+    <DataTable :config="config" :rows="accountStore.accounts">
+      <template #actions="{ row }">
+        <EditCreateAccount :account="row">
+          <template #button>
+            <PencilIcon class="h-6 w-6 cursor-pointer hover:text-blue-600 text-gray-300" />
+          </template>
+        </EditCreateAccount>
+      </template>
+    </DataTable>
+  </template>
+  <template v-else>
+    <div class="text-center">No Accounts</div>
+  </template>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAccountStore, useTransactionStore } from '@/store';
+import { Screens } from '@/utils/screens';
+import { computed, reactive } from 'vue';
+import DataTable from '../components/DataTable.vue';
+import HeadlineActions from '../components/HeadlineActions.vue';
+import { useI18n } from 'vue-i18n';
+import EditCreateAccount from '@components/EditCreateAccount.vue';
+import { PencilIcon, TrashIcon } from '@heroicons/vue/solid';
+import { Account } from '@models';
+
+const { t } = useI18n();
+
+const config = reactive({
+  fields: {
+    name: {},
+    currency: {},
+  },
+  tableLayout: {
+    [Screens.DEFAULT]: [['name'], ['currency']],
+  },
+  settings: {
+    actions: true,
+    translationPrefix: 'accounts.table',
+    sort: {
+      field: 'name',
+      direction: 'desc',
+    },
+  },
+  filters: {},
+});
+
+const accountStore = useAccountStore();
+const transactionStore = useTransactionStore();
+
+await transactionStore.subscribe();
+await accountStore.subscribe();
+
+const rows = computed(() => accountStore.accounts);
+</script>
