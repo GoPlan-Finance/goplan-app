147849ccdea8b62b4e1a450f007e23d4278a2e7c
mkitzmann <mkitzmann@gmail.com>
2022-01-09 08:57:03 +0100

    Fix Watchlist.ts

--- common/src/models/Watchlist.ts
+++ common/src/models/Watchlist.ts
@@ -1,23 +1,25 @@
 import { User } from '@common/models/User';
-import { BaseObject } from '@goplan-finance/utils';
+import { BaseObject, Query } from '@goplan-finance/utils';
 import { AssetSymbol } from './AssetSymbol';
+import { WatchlistItem } from '@models/WatchlistItem';
 
 export class Watchlist extends BaseObject {
   static className = 'Watchlist';
 
   public percentChange: number;
   public symbolsCount: number;
+  public symbols: AssetSymbol[];
 
   constructor() {
     super(Watchlist.className);
   }
 
-  get id(): string {
-    return this.get('id');
-  }
-
   get name(): string {
     return this.get('name');
+  }
+
+  set name(value: string) {
+    this.set('name', value);
   }
 
   get createdBy(): User {
