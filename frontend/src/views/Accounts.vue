dfd6b1041e3b1a243a0291947e6e831314eec1fa
mkitzmann <mkitzmann@gmail.com>
2022-01-09 17:55:24 +0100

    Add balance to Accounts view

--- frontend/src/views/Accounts.vue
+++ frontend/src/views/Accounts.vue
@@ -3,7 +3,7 @@
     <EditCreateAccount />
   </HeadlineActions>
   <template v-if="rows.length > 0">
-    <DataTable :config="config" :rows="accountStore.accounts">
+    <DataTable :config="config" :rows="rows">
       <template #actions="{ row }">
         <EditCreateAccount :account="row">
           <template #button>
@@ -28,16 +28,43 @@ import { useI18n } from 'vue-i18n';
 import EditCreateAccount from '@components/EditCreateAccount.vue';
 import { PencilIcon, TrashIcon } from '@heroicons/vue/solid';
 import { Account } from '@models';
+import { TableConfig } from '@components/DataTable';
+import { TransactionType } from '@models/Transaction';
 
 const { t } = useI18n();
 
-const config = reactive({
+const accountStore = useAccountStore();
+const transactionStore = useTransactionStore();
+
+await transactionStore.subscribe();
+await accountStore.subscribe();
+
+const rows = computed(() => accountStore.accounts);
+const transactions = computed(() => transactionStore.transactions);
+
+const config = reactive<TableConfig>({
   fields: {
     name: {},
-    currency: {},
+    balance: {
+      justify: 'right',
+      format: 'currency',
+      value: (account: Account) => {
+        return transactions.value.reduce((result, currentTransaction) => {
+          if (currentTransaction.account.id === account.id) {
+            if (currentTransaction.type.toLowerCase() === TransactionType.SELL) {
+              return result + currentTransaction.totalExcludingFees;
+            } else if (currentTransaction.type.toLowerCase() === TransactionType.BUY) {
+              return result - currentTransaction.totalExcludingFees;
+            }
+          } else {
+            return result;
+          }
+        }, 0);
+      },
+    },
   },
   tableLayout: {
-    [Screens.DEFAULT]: [['name'], ['currency']],
+    [Screens.DEFAULT]: [['name'], ['balance']],
   },
   settings: {
     actions: true,
@@ -49,12 +76,4 @@ const config = reactive({
   },
   filters: {},
 });
-
-const accountStore = useAccountStore();
-const transactionStore = useTransactionStore();
-
-await transactionStore.subscribe();
-await accountStore.subscribe();
-
-const rows = computed(() => accountStore.accounts);
 </script>
