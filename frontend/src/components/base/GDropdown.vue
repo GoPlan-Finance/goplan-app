031df284e4fad54acee526e4ee27a8b5e2271e77
mkitzmann <mkitzmann@gmail.com>
2022-01-11 08:55:31 +0100

    Add Dropdown component

---
+++ frontend/src/components/base/GDropdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative">
+    <slot name="button" :toggle="toggleDropdown" />
+    <div
+      v-show="dropdownOpen"
+      class="fixed inset-0 h-full w-full z-10"
+      @click="dropdownOpen = false"
+    />
+    <div
+      v-show="dropdownOpen"
+      class="absolute right-0 mt-2 py-2 w-48 bg-white rounded-lg shadow-xl z-20"
+    >
+      <slot :toggle="toggleDropdown" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const dropdownOpen = ref(false);
+
+const toggleDropdown = () => {
+  dropdownOpen.value = !dropdownOpen.value;
+};
+</script>
